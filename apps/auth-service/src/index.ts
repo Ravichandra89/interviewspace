6fb9d3f587feed14d112fa62c67c258a942b0ecd
Ravi chandra <githubravi01@gmail.com>
2025-07-03 22:13:15 +0530

    Turbo Repo Setup

---
+++ apps/auth-service/src/index.ts
@@ -0,0 +1,16 @@
+// import express from "express";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+
+// const app = express();
+// const PORT = process.env.PORT || 4000;
+
+// // Middlewares uses
+// app.use(express.json());
+// app.use(express.urlencoded({ extended : true }));
+// app.use(cookieParser());
+// app.use(cors());
+
+// //Define the Auth Router Here
+// app.use('/api/v1/auth', )
+
