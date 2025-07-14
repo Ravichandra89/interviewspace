00a858f3d7905dbe35cd9c119f1f674e5cf24df9
Ravi chandra <githubravi01@gmail.com>
2025-07-14 20:52:50 +0530

    Problem Router

--- apps/auth-service/src/routes/problem.route.ts
+++ apps/auth-service/src/routes/problem.route.ts
@@ -1,9 +1,26 @@
 import { Router } from "express";
-import createProblem from "../controller/problem.controller";
+import createProblem, {
+  listProblems,
+  getProblem,
+  updateProblem,
+  deleteProblem,
+} from "../controller/problem.controller";
 import validateToken from "../middleware/validateToken";
 
 const problemRouter = Router();
 
 problemRouter.post("/session/:sessionId/create", validateToken, createProblem);
+
+// Route to list all problems
+problemRouter.get("/problems", validateToken, listProblems);
+
+// Route to get a specific problem by ID
+problemRouter.get("/problem/:id", validateToken, getProblem);
+
+// Route to update a specific problem by ID
+problemRouter.put("/problem/:id", validateToken, updateProblem);
+
+// Route to delete a specific problem by Id
+problemRouter.delete("/problem/:id", validateToken, deleteProblem);
 
 export default problemRouter;
